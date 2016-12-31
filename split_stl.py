969f552a92d5b9f6078509d9713f6827028c0050
Ed Brannin <edbrannin@gmail.com>
2016-12-31 01:09:22 -0500

    Separated split/join commands

--- split_stl.py
+++ split_stl.py
@@ -1,6 +1,7 @@
+import os
+
 import click
 import stl
-from path import Path
 
 def split(stl_data):
     groups = list()
@@ -37,7 +38,12 @@ class FacetGroup(object):
 
     def add(self, facet):
         self.facets.append(facet)
-        self.vertices = self.vertices.union(facet.vertices)
+        if "vertices" in dir(facet):
+            self.vertices = self.vertices.union(facet.vertices)
+
+    def add_all(self, facets):
+        for facet in facets:
+            self.add(facet)
 
     def __contains__(self, facet):
         return self.vertices.intersection(set(facet.vertices))
@@ -46,15 +52,37 @@ class FacetGroup(object):
         self.facets.extend(other.facets)
         self.vertices = self.vertices.union(other.vertices)
 
+    def to_solid(self):
+        return stl.Solid(facets=self.facets)
 
-@click.command()
+
+@click.group()
+def main():
+    pass
+
+@main.command()
 @click.argument("filename")
-def main(filename):
+def split(filename):
     with open(filename) as f:
         data = stl.read_ascii_file(f)
-        print len(data.facets)
         groups = split(data)
-        print len(groups)
+        print "Before: {}; after: {}".format(len(data.facets), len(groups))
+        for i, group in enumerate(groups):
+            out_name = "{}.{}.stl".format(os.path.splitext(filename)[0], i)
+            with open(out_name, 'w') as out_file:
+                group.to_solid().write_ascii(out_file)
+                print "Wrote {}".format(out_name)
+
+
+@main.command()
+@click.argument("files", type=click.File(), nargs=-1)
+@click.option("-o", "--out", default="-", type=click.File("w"))
+def join(files, out):
+    group = FacetGroup()
+    for f in files:
+        data = stl.read_ascii_file(f)
+        group.add_all(data.facets)
+    group.to_solid().write_ascii(out)
 
 if __name__ == '__main__':
     main()
